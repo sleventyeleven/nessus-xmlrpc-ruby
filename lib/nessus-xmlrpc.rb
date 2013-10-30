52a4ea440a04b85701f19cdc3bbee79ffd63f0fd
Michael Contino <Cont512n02@live.com>
2013-10-29 20:34:30 -0400

    changed sleep time to avoid losing session

--- lib/nessus-xmlrpc.rb
+++ lib/nessus-xmlrpc.rb
@@ -575,8 +575,7 @@ class NessusXMLRPCnokogiri < NessusXMLRPCrexml
                 doc = Nokogiri::HTML::DocumentFragment.parse(http_content)
                 str = doc.at_css('meta[http-equiv="refresh"]')['content']
                 file_name = str[/\?fileName=(.*)/,1]
-				$stderr.print "[i] waiting 30 seconds to ensure report is aviable"
-				sleep 30
+		sleep 10
                 post2= { "fileName" => file_name }
                 file=nessus_http_request('file/xslt/download', post2)
                 return file
